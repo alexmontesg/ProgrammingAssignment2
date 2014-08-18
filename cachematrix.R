fb1ad143eb1af644d41de5ff08f02dffa5b8b005
Alejandro Montes <alejandro.montes.g@gmail.com>
2014-08-18 11:36:56 +0200

    Added function to create matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,23 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions can be used to efficiently
+## calculate the inverse of a matrix several
+## times, because its value is cached and it is
+## only calculated once
 
-## Write a short comment describing this function
+## Creates a list of functions to get/set the
+## value of a matrix as well as its mean
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setInv <- function(inverse) inv <<- inverse
+    getInv <- function() inv
+    
+    list(set = set, get = get, setInv = setInv, 
+         getInv = getInv)
 }
 
 
